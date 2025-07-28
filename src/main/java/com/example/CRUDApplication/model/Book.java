90e1ce918f44fe7ed7f5ef282e3e38e060dd12f4
samsur-acct <samsur.acct@gmail.com>
2025-07-27 20:02:42 -0400

    add spotless and error msg

--- src/main/java/com/example/CRUDApplication/model/Book.java
+++ src/main/java/com/example/CRUDApplication/model/Book.java
@@ -7,18 +7,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
 
-
 @Entity
-@Table(name="Books")
+@Table(name = "Books")
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
 @ToString
 public class Book {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String title;
-    private String author;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String title;
+  private String author;
 }
--- src/main/java/com/example/CRUDApplication/repo/BookRepo.java
+++ src/main/java/com/example/CRUDApplication/repo/BookRepo.java
@@ -1,10 +1,8 @@
 package com.example.CRUDApplication.repo;
 
+import com.example.CRUDApplication.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.CRUDApplication.model.Book;
 
 @Repository
-public interface BookRepo extends JpaRepository<Book, Long>{
-
-}
+public interface BookRepo extends JpaRepository<Book, Long> {}
